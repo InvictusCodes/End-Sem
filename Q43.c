0c04fb95159b2a350ccb60d880f67e2cf28f7c0a
Vishal Bambhaniya <invictuscodes@gmail.com>
2025-06-07 01:08:18 +0530

    Create Q43.c

---
+++ Q43.c
@@ -0,0 +1,35 @@
+WAP to cunt the nmber of digits , alphabets ,and special characters in an Input string.
+Code:
+#include <stdio.h>
+
+int main() {
+    int digits = 0, alpha = 0, special = 0, i;
+    char str[120];
+
+    printf("Enter a String: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        char ch = str[i];
+
+        if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+        {
+            alpha++;
+        }
+        else if(ch >= '0' && ch <= '9') 
+        {
+            digits++;
+        }
+        else if(ch != ' ' && ch != '\n') 
+        {
+            special++;
+        }
+    }
+
+    printf("Alphabets: %d\n", alpha);
+    printf("Digits: %d\n", digits);
+    printf("Special Characters: %d\n", special);
+
+    return 0;
+}
